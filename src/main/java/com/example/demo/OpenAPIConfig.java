66e013e2e0e3f65738b3c154719132be589532af
samlogy <aceportgasd235@gmail.com>
2023-06-15 15:53:04 +0100

    add: swagger API

---
+++ src/main/java/com/example/demo/OpenAPIConfig.java
@@ -0,0 +1,38 @@
+package com.example.demo;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+
+@Configuration
+public class OpenAPIConfig {
+
+    @Value("http://localhost:8080")
+    private String devUrl;
+
+    @Bean
+    public OpenAPI myOpenAPI() {
+        Server devServer = new Server();
+        devServer.setUrl(devUrl);
+        devServer.setDescription("Server URL in Development environment");
+
+        Contact contact = new Contact();
+        contact.setEmail("senanisammy@gmail.com");
+        contact.setName("Samlogy");
+
+        Info info = new Info()
+                .title("Java Spring API")
+                .version("1.0")
+                .contact(contact)
+                .description("This API exposes --> Spring Learning :)");
+
+        return new OpenAPI().info(info).servers(List.of(devServer));
+    }
+}
