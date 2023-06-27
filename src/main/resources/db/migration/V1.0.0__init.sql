8a8014da9f858f94b1a8ebffdcd3b346201d6db3
samlogy <aceportgasd235@gmail.com>
2023-06-27 10:35:45 +0100

    edit: all sql requests (migrations)

--- src/main/resources/db/migration/V1.0.0__init.sql
+++ src/main/resources/db/migration/V1.0.0__init.sql
@@ -1,19 +1,16 @@
-CREATE TABLE  meeting (
-  id UUID PRIMARY KEY,
-  creneau_horaire VARCHAR(255),
-  meeting_type VARCHAR(50),
-  room_id UUID,
-  created_at DATE
-);
-
 CREATE TABLE room (
-  id UUID PRIMARY KEY,
+  roomId UUID PRIMARY KEY,
   name VARCHAR(255),
   capacity INTEGER,
-  resources VARCHAR(50),
-  created_at DATE
+  resources VARCHAR(255),
+  createdAt DATE
 );
 
-ALTER TABLE meeting
-ADD CONSTRAINT fk_room_id
-FOREIGN KEY (room_id) REFERENCES room (id);
+CREATE TABLE booking (
+  bookingId UUID PRIMARY KEY,
+  creneauHoraire VARCHAR(255),
+  meetingType VARCHAR(50),
+  roomId UUID,
+  createdAt DATE,
+  FOREIGN KEY (roomId) REFERENCES room (roomId)
+ );
