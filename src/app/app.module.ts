259d4282069f260ea91105fdd0db21f98fd74611
samlogy <aceportgasd235@gmail.com>
2023-06-28 08:13:12 +0100

    add: * nav bar * routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,24 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { AddComponent } from './add/add.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FilterComponent } from './filter/filter.component';
+import { RouterModule, Routes } from '@angular/router';
+import appRoutes from './routerConfig';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AnnonceDetailsComponent } from './annonce-details/annonce-details.component';
 
 @NgModule({
-  declarations: [AppComponent, FilterComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  declarations: [AppComponent, FilterComponent, AddComponent, PageNotFoundComponent, AnnonceDetailsComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    RouterModule,
+    RouterModule.forRoot(appRoutes),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/routerConfig.ts
@@ -0,0 +1,22 @@
+import { Routes } from '@angular/router';
+import { AddComponent } from './add/add.component';
+import { FilterComponent } from './filter/filter.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AnnonceDetailsComponent } from './annonce-details/annonce-details.component';
+
+const appRoutes: Routes = [
+  {
+    path: 'annonce/filter',
+    component: FilterComponent,
+  },
+  {
+    path: 'annonce/add',
+    component: AddComponent,
+  },
+  { path: 'annonce/:id', component: AnnonceDetailsComponent },
+
+  { path: '', redirectTo: 'annonce/filter', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent },
+];
+
+export default appRoutes;
