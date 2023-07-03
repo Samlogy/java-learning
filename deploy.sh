16176817976b7c8dc0f1081cb36826300c99eb22
samlogy <aceportgasd235@gmail.com>
2023-07-03 17:45:45 +0100

    wcwc

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
-docker build -t angular .
-docker tag  senanisammy@gmail.com/angular:1.0
-docker push senanisammy@gmail.com/angular:1.0
+docker build -t angular-app .
+docker tag  senanisammy@gmail.com/angular-app:1.0
+docker push senanisammy@gmail.com/angular-app:1.0
